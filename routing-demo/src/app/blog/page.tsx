6c6d9e0cdb265249488024ac8e626cf24bc1f150
newdevmike22 <paola@dominicansoul.com>
2026-08-16 11:19:07 -0400

    Using loading.tsx
    Using a loading.tsx file.

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,5 +1,17 @@
-const Blog = () => {
-  return <h1>My Blog</h1>;
+import { Metadata } from "next";
+import async from "./../products/[productId]/reviews/[reviewId]/page";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog",
+  },
 };
 
-export default Blog;
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("intentional delay");
+    }, 2000);
+  });
+  return <h1>My Blog</h1>;
+}
