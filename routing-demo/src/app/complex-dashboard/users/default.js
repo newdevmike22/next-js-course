5597264206590e8d311f0fcc41a049f1d3ee1443
newdevmike22 <paola@dominicansoul.com>
2026-08-17 13:30:08 -0400

    Parallel routes
    Setting up and using parallel routes.

---
+++ routing-demo/src/app/complex-dashboard/users/default.js
@@ -0,0 +1,9 @@
+export default function DefaultNotifications() {
+  return (
+    <div>
+      {/* Add your default fallback UI here */}
+      <h2>Notifications</h2>
+      <p>No new notifications.</p>
+    </div>
+  );
+}
